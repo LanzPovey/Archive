81c813e39942c704bf1d846c721c6c7a2ba0d01d
LanzPovey <40040682+LanzPovey@users.noreply.github.com>
2022-08-04 13:16:53 -0500

    11

---
+++ tester.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+import testing.advancedclasses.AdvancedClassesTest;
+// didn't have to importh with recursion test, since they were both in the same package.
+
+/**
+* tester.java - tests tests
+* @author Lanz Povey
+* @since Feb 21st 2018
+* @instructor Mr. Wachs
+*/
+public class tester
+{
+    public tester()
+    {
+        // constructor method
+        //RecursionTest unit1 = new RecursionTest();
+        AdvancedClassesTest unit2 = new AdvancedClassesTest();
+    }
+}
